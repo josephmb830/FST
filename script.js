57eb89f9a57c34860652d2fb547d3c4ec4be0afe
joseph <josephmoran@duck.com>
2023-12-19 19:36:41 -0500

    se agrego imagen

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 document.getElementById('generatePdf').addEventListener('click', function () {
     // Crear una instancia de jsPDF
-    var pdf = new jsPDF();
+    const { jsPDF } = jspdf;
+    const pdf = new jsPDF();
+    const image = new Image();
     
-     // Agregar título centrado
+   // Agregar logo a la izquierda del título
+   image.src = 'https://hips.hearstapps.com/hmg-prod/images/beautiful-smooth-haired-red-cat-lies-on-the-sofa-royalty-free-image-1678488026.jpg?crop=0.88847xw:1xh;center,top&resize=1200:*'; // Reemplaza con la URL de tu imagen de gato izquierdo
+   pdf.addImage(image, 'PNG', 10, 10, 40, 40); // Ajusta las coordenadas y el tamaño según tus necesidades
 
     // Agregar título centrado
-    var title = "AÑO DEL FORTALECIMIENTO DE LA SOBERANÍA NACIONAL";
+    var title = "MUNICIPALIDAD DISTRITAL MAGDALENA";
+    pdf.setFont("helvetica", "bold");
     var titleWidth = pdf.getStringUnitWidth(title) * pdf.internal.getFontSize() / pdf.internal.scaleFactor;
     var x = (pdf.internal.pageSize.width - titleWidth) / 2;
     pdf.text(title, x, 20);
